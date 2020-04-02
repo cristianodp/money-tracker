a715ba2e5c20ef8bf130d0d731c320fdcca0eb67
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2020-04-02 10:57:31 +0200

    #3 stating to implementing UI

---
+++ android/app/src/main/java/com/cdp/moneytracker/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.cdp.moneytracker
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_movements.*
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home , container, false)
+    }
+
+}
